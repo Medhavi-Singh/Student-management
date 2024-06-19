0ad0b9b66db202e97ed7a2e61c4b215cc6ad7de7
Medhavi Singh <medhavi.singh@innostax.com>
2024-06-19 18:43:15 +0530

    created store

---
+++ my-app/src/app/UserReducer.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: "users",
+    initialState:{name: "", username: "", email: "", website: "", phone: ""},
+    reducers:{ 
+    }
+})
